c825bf7e70bcddbe9e0ce58c0cfecefae4f79543
TrivialBrett <fbtthrasher@gmail.com>
2024-02-04 11:34:27 -0600

    Added custom tooltips

--- src/main/java/net/TrivialBrett/mccourse/block/custom/SoundBlock.java
+++ src/main/java/net/TrivialBrett/mccourse/block/custom/SoundBlock.java
@@ -2,6 +2,7 @@ package net.TrivialBrett.mccourse.block.custom;
 
 import net.TrivialBrett.mccourse.MCCourseMod;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -9,10 +10,16 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Properties pProperties)
@@ -38,6 +45,12 @@ public class SoundBlock extends Block {
         }
 
         return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.translatable("tooltip.mccourse.sound_block"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 
     @Override
--- src/main/java/net/TrivialBrett/mccourse/item/custom/MetalDetectorItem.java
+++ src/main/java/net/TrivialBrett/mccourse/item/custom/MetalDetectorItem.java
@@ -2,16 +2,23 @@ package net.TrivialBrett.mccourse.item.custom;
 
 import net.TrivialBrett.mccourse.MCCourseMod;
 import net.TrivialBrett.mccourse.block.ModBlocks;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties)
@@ -51,6 +58,20 @@ public class MetalDetectorItem extends Item {
                 player -> player.broadcastBreakEvent(player.getUsedItemHand()));
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced)
+    {
+        if(Screen.hasShiftDown())
+        {
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.metal_detector.tooltip.shift"));
+
+        }else {
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.metal_detector.tooltip"));
+        }
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private void outputNoValuableFound(Player player)
