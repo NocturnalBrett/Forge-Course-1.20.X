cf61e4996a72c2608e1aa4236c4834f97559b836
TrivialBrett <fbtthrasher@gmail.com>
2024-02-23 13:21:37 -0600

    Added Alexandrite Bow

--- src/main/java/net/TrivialBrett/mccourse/item/ModItems.java
+++ src/main/java/net/TrivialBrett/mccourse/item/ModItems.java
@@ -76,6 +76,9 @@ public class ModItems {
     public static final RegistryObject<Item> RADIATION_STAFF = ITEMS.register("radiation_staff",
             () -> new Item(new Item.Properties().durability(1024)));
 
+    public static final RegistryObject<Item> ALEXANDRITE_BOW = ITEMS.register("alexandrite_bow",
+            () -> new BowItem(new Item.Properties().durability(500)));
+
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
